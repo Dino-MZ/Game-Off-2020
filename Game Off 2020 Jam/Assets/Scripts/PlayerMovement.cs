bd86d7bd620f7f1966fa512c75a5ea39c5122c89
MZ-Dino <38118953+Dino-MZ@users.noreply.github.com>
2020-11-21 14:11:14 -0600

    Movement finished
    Its feeling prettey smooth

--- Game Off 2020 Jam/Assets/Scripts/PlayerMovement.cs
+++ Game Off 2020 Jam/Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,7 @@ public class PlayerMovement : MonoBehaviour
     public SpriteRenderer pSprite;
 
     public float pSpeed;
-    
+    public float fHorizontalDamping = 0.5f;
 
     private float moveHor;
 
@@ -37,5 +37,12 @@ public class PlayerMovement : MonoBehaviour
             rb.AddForce(transform.right * -pSpeed);
             pSprite.flipX = true;
         }
+
+        // speeding up
+        float fHorizontalVelocity = rb.velocity.x;
+        fHorizontalVelocity += Input.GetAxisRaw("Horizontal");
+        fHorizontalVelocity *= Mathf.Pow(1f - fHorizontalDamping, Time.deltaTime * 10f);
+        rb.velocity = new Vector2(fHorizontalVelocity, rb.velocity.y);
+
     }
 }
